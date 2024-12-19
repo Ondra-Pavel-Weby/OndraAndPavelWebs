923241b3cc03d697f9e50c2b9887d144c455b6b4
Pavel Olyšar <olysarp@gmail.com>
2024-12-19 06:51:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# WebAgencyWebsite
+
+- This is a frontend for Ondra & Pavel Weby, the web agency we started.
+
+## Contributors
+
+- [Ondra's github](https://github.com/0NDRS)
+- [Pavel's github](https://github.com/PavelOlysar)
---
+++ js/index.js
@@ -0,0 +1,18 @@
+// --- Scroll To Top Button ---
+let scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.classList.add('show');
+  } else {
+    scrollToTopBtn.classList.remove('show');
+  }
+}
+
+scrollToTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
